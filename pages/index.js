4a3da5035248a8a99a2542321666c2314605e840
Eduardo Ribeiro <dudarib@outlook.com>
2024-03-15 16:45:23 +0000

    add 'NFTCard and Filter' sections

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category } from '../components/componentsindex';
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category, Filter, NFTCard } from '../components/componentsindex';
 
 const Home = () => {
   return (
@@ -10,6 +10,12 @@ const Home = () => {
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title 
+      heading="Featured NFTs"
+      paragraph="Discover the most outstanding NFTs."
+      />
+      <Filter />
+      <NFTCard />
       <Title 
       heading="Browse by category"
       paragraph="Explore the NFTs in the most featured categories"
